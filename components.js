3db16203a1fa072461aaa0629413d69dd0e4959f
Adhvaith Rajesh <rajesh.adhvaith@gmail.com>
2026-05-15 22:54:13 +0530

    make blog-entry card clickable

--- components.js
+++ components.js
@@ -3,15 +3,18 @@ class blogEntry extends HTMLElement {
         const title = this.getAttribute("title");
         const date = this.getAttribute("date");
         const description = this.getAttribute("description");
+        const href = this.getAttribute("href");
 
         this.innerHTML = `
-        <div class="blog-entry">
-            <div class="blog-entry-header">
-                <h2 class="blog-entry-title">${title}</h2>
-                <h3 class="blog-entry-date">${date}</h3>
+        <a href="${href}" class="blog-entry-link">
+            <div class="blog-entry">
+                <div class="blog-entry-header">
+                    <h2 class="blog-entry-title">${title}</h2>
+                    <h3 class="blog-entry-date">${date}</h3>
+                </div>
+                <span>${description}</span>
             </div>
-            <span>${description}</span>
-        </div>
+        </a>
         `
     }
 }
